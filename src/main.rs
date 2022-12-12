606a391e746be3005d6f6c2c89c68415caa10674
TheRealKeto <therealketo@gmail.com>
2022-12-11 20:48:00 -0500

    feat!: Introduce intial POC
    * Introduce general proof-of-concept
    
    * Introduce structures to deserialize responses
    
    Signed-off-by: TheRealKeto <therealketo@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+//
+// Copyright (c) 2022, Keto (TheRealKeto)
+// SPDX-License-Identifier: BSD-3-Clause
+//
+use serde::Deserialize;
+use reqwest::header::USER_AGENT;
+
+#[derive(Debug, Deserialize)]
+struct CanisterAPIResponse {
+    status: String,
+    date: String,
+    message: Option<String>,
+    data: Vec<CanisterPackage>
+}
+
+#[derive(Debug, Deserialize)]
+struct CanisterPackage {
+    identifier: String,
+    name: Option<String>,
+    description: String,
+    author: Option<String>,
+    maintainer: String,
+    depiction: Option<String>,
+    sileo_depiction: Option<String>,
+    icon_url: Option<String>,
+    repository: CanisterPackageRepo,
+    color: Option<String>
+}
+
+#[derive(Debug, Deserialize)]
+struct CanisterPackageRepo {
+    uri: String
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Setup a base URL for the Canister API
+    // then, include a valid endpoint inside
+    let base_url = "https://api.canister.me/v1/community";
+    let request_url = format!("{}/{}", base_url, "packages/search");
+
+    // Start setting up the API request
+    // then, retrieve the response
+    let client = reqwest::Client::new();
+    let request = client
+        .get(request_url)
+        .header(USER_AGENT, "TheRealKeto/canister-rs")
+        .query(&[("query", "siguza-utils")])
+        .send()
+        .await?;
+
+    let resp = request.json::<CanisterAPIResponse>().await?;
+    println!("{:#?}", resp);
+    Ok(())
 }
