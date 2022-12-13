62b9086577834b4d47452fc27a7688a9b8057c78
TheRealKeto <therealketo@gmail.com>
2022-12-13 13:47:00 -0500

    feat(lib): Introduce blocking support
    * Introduce blocking support
    
    * Add flags to handle features and imports
    
    * Update dependency options in manifest
    
    Signed-off-by: TheRealKeto <therealketo@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,10 @@
 pub mod models;
 use models::CanisterAPIResponse;
 
-use reqwest::header;
+#[cfg(feature = "async")]
+use reqwest::{header, Client};
+#[cfg(not(feature = "async"))]
+use reqwest::{header, blocking::Client};
 
 #[derive(thiserror::Error, Debug)]
 #[non_exhaustive]
@@ -19,20 +22,25 @@ pub enum CanisterAPIError {
 // Model of a Canister client
 #[derive(Debug)]
 pub struct Canister {
+    client: Client,
     user_agent: String,
-    client: reqwest::Client,
 }
+
+// Base URL of all Canister API requests
+// TODO: Migrate to v2 at some point in time
+const BASE_URL: &str = "https://api.canister.me/v1/community";
 
 impl Canister {
     // Creates a new instance of the client
     pub fn new(user_agent: &str) -> Canister {
         Canister {
+            client: Client::new(),
             user_agent: user_agent.to_string(),
-            client: reqwest::Client::new()
         }
     }
 
-    // Error handling for failed request
+    // Handle errors based on status codes
+    // Bad requests are pretty terrible on v1
     fn map_response_err(&self, code: String) -> CanisterAPIError {
         match code.as_str() {
             "400: Bad Request" => CanisterAPIError::BadRequest("Bad request"),
@@ -40,15 +48,33 @@ impl Canister {
         }
     }
 
+    #[cfg(not(feature = "async"))]
+    pub fn search_canister(
+        &self, endpoint: &str, query: &str
+    ) -> Result<CanisterAPIResponse, CanisterAPIError> {
+        let request_url = format!("{}/{}", BASE_URL, endpoint);
+        let request = self.client
+            .get(request_url)
+            .header(header::USER_AGENT, &self.user_agent)
+            .query(&[("query", query)])
+            .build()?;
+
+        let response: CanisterAPIResponse = self.client
+            .execute(request)?
+            .json()?;
+
+        match response.status.as_str() {
+            "Successful" => Ok(response),
+            _ => Err(self.map_response_err(response.status))
+        }
+    }
+
+    // Develop a differenet function for async support
+    #[cfg(feature = "async")]
     pub async fn search_canister(
         &self, endpoint: &str, query: &str
     ) -> Result<CanisterAPIResponse, CanisterAPIError> {
-        // Setup a base URL for the Canister API
-        // then, append the given endpoint to it
-        let base_url = "https://api.canister.me/v1/community";
-        let request_url = format!("{}/{}", base_url, endpoint);
-
-        // Make a request and get a response
+        let request_url = format!("{}/{}", BASE_URL, endpoint);
         let request = self.client
             .get(request_url)
             .header(header::USER_AGENT, &self.user_agent)
@@ -61,7 +87,6 @@ impl Canister {
             .json()
             .await?;
 
-        // Safely parse the response and handle errors
         match response.status.as_str() {
             "Successful" => Ok(response),
             _ => Err(self.map_response_err(response.status))
